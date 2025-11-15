06ededd9aa88d825a8f0e46023e07bbe046e7af2
kim001hs <kim001hs@gmail.com>
2025-11-15 17:54:25 +0900

    create_done

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -27,6 +27,8 @@ static unsigned s_tell(int fd);
 static void s_close(int fd);
 // extra
 static int s_dup2(int oldfd, int newfd);
+
+static void s_check_access(const char *);
 /* System call.
  *
  * Previously system call services was handled by the interrupt handler
@@ -168,6 +170,8 @@ static int s_exec(const char *file)
 		return -1;
 	strlcpy(fn_copy, file, PGSIZE);
 
+	s_check_access(fn_copy);
+
 	int res = process_exec(fn_copy);
 	// palloc_free_page(fn_copy); free는 process_exec안에서
 	return res;
@@ -198,10 +202,9 @@ static int s_wait(int tid)
 
 static bool s_create(const char *file, unsigned initial_size)
 {
-	// 	크기 `initial_size`의 새 파일을 생성합니다. 성공 시 `true`, 실패 시 `false` 반환.
+	s_check_access(file);
 
-	// > 생성은 열기와 다릅니다. 열려면 open() 호출 필요.
-	// process_create_initd(file);
+	return filesys_create(file, initial_size);
 }
 
 static bool s_remove(const char *file)
@@ -272,4 +275,12 @@ static int s_dup2(int oldfd, int newfd)
 	// - 복제된 디스크립터는 **파일 오프셋과 상태 플래그를 공유**합니다.
 	// 	- 예: `seek()`으로 하나의 파일 위치를 바꾸면, 다른 디스크립터도 같은 위치를 가리킵니다.
 	// - **`fork()` 이후에도 dup된 fd의 의미는 유지되어야 합니다.**
+}
+
+static void s_check_access(const char *fn_copy)
+{
+	if (fn_copy == NULL || !is_user_vaddr(fn_copy) || pml4_get_page(thread_current()->pml4, fn_copy) == NULL)
+	{
+		s_exit(-1);
+	}
 }
