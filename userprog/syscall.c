e97ef363a079134c32a062c90563835ca3011f0a
kim001hs <kim001hs@gmail.com>
2025-11-17 22:00:59 +0900

    filesize

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -266,7 +266,7 @@ static int s_open(const char *file)
 static int s_filesize(int fd)
 {
 	// 열려 있는 파일의 크기를 바이트 단위로 반환합니다.
-	return 373;
+	return file_length(thread_current()->fd_table[fd]);
 }
 
 static int s_read(int fd, void *buffer, unsigned length)
